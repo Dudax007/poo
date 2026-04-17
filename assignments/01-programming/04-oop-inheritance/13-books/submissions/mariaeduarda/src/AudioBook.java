09b8b9966a65abc4843eb0c8920e6c28e46b86b3
Maria Eduarda <mariaeduardaandrade228@gmail.com>
2026-04-17 10:54:25 -0300

    corrigida

--- assignments/01-programming/04-oop-inheritance/13-books/submissions/mariaeduarda/src/AudioBook.java
+++ assignments/01-programming/04-oop-inheritance/13-books/submissions/mariaeduarda/src/AudioBook.java
@@ -27,32 +27,3 @@ public class AudioBook extends Book {
         return narrator;
     }
 }
-public class AudioBook extends Book {
-    private double fileSizeInMB;
-    private int playLengthInMinutes;
-    private String narrator;
-
-    public AudioBook(String title, int year, String author, double fileSizeInMB, int playLengthInMinutes, String narrator) {
-        super(title, year, author);
-        this.fileSizeInMB = fileSizeInMB;
-        this.playLengthInMinutes = playLengthInMinutes;
-        this.narrator = narrator;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + String.format(" | File Size: %.2f MB | Length: %d minutes | Narrator: %s", fileSizeInMB, playLengthInMinutes, narrator);
-    }
-
-    public double getFileSizeInMB() {
-        return fileSizeInMB;
-    }
-
-    public int getPlayLengthInMinutes() {
-        return playLengthInMinutes;
-    }
-
-    public String getNarrator() {
-        return narrator;
-    }
-}
--- assignments/01-programming/04-oop-inheritance/13-books/submissions/mariaeduarda/src/PrintBook.java
+++ assignments/01-programming/04-oop-inheritance/13-books/submissions/mariaeduarda/src/PrintBook.java
@@ -21,26 +21,3 @@ public class PrintBook extends Book {
         return isbn;
     }
 }
-public class PrintBook extends Book {
-    private String publisher;
-    private String isbn;
-
-    public PrintBook(String title, int year, String author, String publisher, String isbn) {
-        super(title, year, author);
-        this.publisher = publisher;
-        this.isbn = isbn;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + String.format(" | Publisher: %s | ISBN: %s", publisher, isbn);
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-}
